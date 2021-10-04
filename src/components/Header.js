21cd12758c54ca25ec6287a2d8b8748a455e64cb
Gabriel Zoya <zoyagabriel@gmail.com>
2021-10-04 17:12:56 -0300

    Adding Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        text={showAdd ? "Close" : "Add"}
-        color={showAdd ? "red" : "blue"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "red" : "blue"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
